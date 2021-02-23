3c21a9adff4ae29ff9132e0cc3e38fad75eb6c08
AnnaBzhnsk <77431971+AnnaBzhnsk@users.noreply.github.com>
2021-02-23 15:49:30 +0300

    Interfaces

---
+++ Practical07-08/EnumPack/EnumDay.java
@@ -0,0 +1,20 @@
+package EnumPack;
+
+public class EnumDay {
+    public static void main(String[] args) {
+
+        Day current = Day.MONDAY;
+        System.out.println(current);    // MONDAY
+    }
+}
+enum Day{
+
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
+
